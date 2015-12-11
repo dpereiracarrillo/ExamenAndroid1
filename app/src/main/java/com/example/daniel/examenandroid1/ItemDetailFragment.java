9838a29f14a87b9b154630772d52aa75830e4407
dpereiracarrillo <dpereiracarrillo@danielcastelao.org>
2015-12-11 12:49:29 +0100

    creado el método y el botón en la parte lógica

--- app/src/main/java/com/example/daniel/examenandroid1/ItemDetailFragment.java
+++ app/src/main/java/com/example/daniel/examenandroid1/ItemDetailFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.daniel.examenandroid1.dummy.DummyContent;
@@ -49,14 +50,23 @@ public class ItemDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
 
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
             ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.details);
         }
+        //creamos el botón para borrar
+        Button boton = (Button) rootView.findViewById(R.id.botonBorrar);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) rootView.findViewById(R.id.item_detail)).setText("");
+            }
+        });
 
 
         return rootView;
     }
 }
+
